867fccb9cfd38e6a01e0179a7e40bf4f52df864d
Steve Clay <steve@mrclay.org>
2008-06-07 23:17:16 +0000

    More ab testing and result files

--- web/ab_tests/ab.bat
+++ web/ab_tests/ab.bat
@@ -1,5 +1,4 @@
-cd C:\xampp\apache\bin
-
-ab -c 100 -n 1000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/minify/before.js.php > results_minify.txt
-ab -c 100 -n 1000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/mod_deflate/before.js > results_deflate.txt
-ab -c 100 -n 1000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/type-map/before.js.var > results_type-map.txt
+C:\xampp\apache\bin\ab -c 100 -n 2000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/ideal_php/before.php > results_ideal_php.txt
+C:\xampp\apache\bin\ab -c 100 -n 2000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/minify/before.js.php > results_minify.txt
+C:\xampp\apache\bin\ab -c 100 -n 2000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/mod_deflate/before.js > results_deflate.txt
+C:\xampp\apache\bin\ab -c 100 -n 2000 -H "Accept-Encoding: deflate, gzip" http://mc.dev/_3rd_party/minify/web/ab_tests/type-map/before.js.var > results_type-map.txt
