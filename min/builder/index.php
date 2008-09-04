d637083d5734756bd4b88158ca0b5005858e6ed8
Steve Clay <steve@mrclay.org>
2008-09-04 02:06:10 +0000

    builder : checks min/config.php for cache path and uploaderHoursBehind

--- min/builder/index.php
+++ min/builder/index.php
@@ -1,10 +1,16 @@
 <?php
 
+require dirname(__FILE__) . '/../config.php';
+
 set_include_path(dirname(__FILE__) . '/../lib' . PATH_SEPARATOR . get_include_path());
 
 require 'Minify.php';
 
-Minify::setCache();
+if (0 === stripos(PHP_OS, 'win')) {
+    Minify::setDocRoot(); // we may be on IIS
+}
+Minify::setCache(isset($min_cachePath) ? $min_cachePath : null);
+Minify::$uploaderHoursBehind = $min_uploaderHoursBehind;
 
 Minify::serve('Page', array(
     'file' => dirname(__FILE__) . '/_index.html'
