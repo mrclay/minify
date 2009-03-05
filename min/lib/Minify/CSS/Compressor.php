e5ded285c09b7ac3b86ae6ec95045db373f59d74
Steve Clay <steve@mrclay.org>
2009-03-05 16:46:47 +0000

    CSS/Compressor.php : fix Issue 95 + test

--- min/lib/Minify/CSS/Compressor.php
+++ min/lib/Minify/CSS/Compressor.php
@@ -147,7 +147,10 @@ class Minify_CSS_Compressor {
             \\s+
             /x'
             ,"$1\n", $css);
-
+        
+        // prevent triggering IE6 bug: http://www.crankygeek.com/ie6pebug/
+        $css = preg_replace('/:first-l(etter|ine)\\{/', ':first-l$1 {', $css);
+            
         return trim($css);
     }
     
