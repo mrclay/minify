d8d1f89ca05b030c44565173846704a7418de7bf
Steve Clay <steve@mrclay.org>
2010-12-19 21:00:27 +0000

    Issue 214 (remove traversals in prepended links)

--- min_unit_tests/test_Minify_CSS_UriRewriter.php
+++ min_unit_tests/test_Minify_CSS_UriRewriter.php
@@ -17,7 +17,7 @@ function test_Minify_CSS_UriRewriter()
         ,$thisDir // use DOCUMENT_ROOT = '/full/path/to/min_unit_tests'
     );
     
-    $passed = assertTrue($expected === $actual, 'Minify_CSS_UriRewriter');
+    $passed = assertTrue($expected === $actual, 'Minify_CSS_UriRewriter : rewrite');
     if (__FILE__ === realpath($_SERVER['SCRIPT_FILENAME'])) {
         echo "\n---Input:\n\n{$in}\n";
         echo "\n---Output: " .countBytes($actual). " bytes\n\n{$actual}\n\n";
@@ -29,6 +29,45 @@ function test_Minify_CSS_UriRewriter()
         echo "--- Minify_CSS_UriRewriter::\$debugText\n\n"
             , Minify_CSS_UriRewriter::$debugText;
     }
+
+
+    Minify_CSS_UriRewriter::$debugText = '';
+    $in = file_get_contents($thisDir . '/_test_files/css_uriRewriter/in.css');
+    $expected = file_get_contents($thisDir . '/_test_files/css_uriRewriter/exp_prepend.css');
+    $actual = Minify_CSS_UriRewriter::prepend($in, 'http://cnd.com/A/B/');
+
+    $passed = assertTrue($expected === $actual, 'Minify_CSS_UriRewriter : prepend1');
+    if (__FILE__ === realpath($_SERVER['SCRIPT_FILENAME'])) {
+        echo "\n---Input:\n\n{$in}\n";
+        echo "\n---Output: " .countBytes($actual). " bytes\n\n{$actual}\n\n";
+        if (!$passed) {
+            echo "---Expected: " .countBytes($expected). " bytes\n\n{$expected}\n\n\n";
+        }
+
+        // show debugging only when test run directly
+        echo "--- Minify_CSS_UriRewriter::\$debugText\n\n"
+            , Minify_CSS_UriRewriter::$debugText;
+    }
+
+
+    Minify_CSS_UriRewriter::$debugText = '';
+    $in = file_get_contents($thisDir . '/_test_files/css_uriRewriter/in.css');
+    $expected = file_get_contents($thisDir . '/_test_files/css_uriRewriter/exp_prepend2.css');
+    $actual = Minify_CSS_UriRewriter::prepend($in, '//cnd.com/A/B/');
+
+    $passed = assertTrue($expected === $actual, 'Minify_CSS_UriRewriter : prepend2');
+    if (__FILE__ === realpath($_SERVER['SCRIPT_FILENAME'])) {
+        echo "\n---Input:\n\n{$in}\n";
+        echo "\n---Output: " .countBytes($actual). " bytes\n\n{$actual}\n\n";
+        if (!$passed) {
+            echo "---Expected: " .countBytes($expected). " bytes\n\n{$expected}\n\n\n";
+        }
+
+        // show debugging only when test run directly
+        echo "--- Minify_CSS_UriRewriter::\$debugText\n\n"
+            , Minify_CSS_UriRewriter::$debugText;
+    }
+
     
     Minify_CSS_UriRewriter::$debugText = '';
     $in = '../../../../assets/skins/sam/sprite.png';
