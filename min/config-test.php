f40f4e4f563b71c5a2d53df74a7dd203c344838b
Steve Clay <steve@mrclay.org>
2012-03-15 14:00:50 -0400

    Easier config testing

---
+++ min/config-test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Additional configuration applied when the variable "test" is added to the querystring.
+ *
+ * To test config options, place them in this file and add "&test" to your Minify URL.
+ * Note that if this is on a public server, anyone can execute your test.
+ * 
+ * @package Minify
+ */
+
