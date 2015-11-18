7af644e3e165334a0b83ee09bae41174347f907e
Elan Ruusamäe <glen@delfi.ee>
2015-11-18 17:50:23 +0200

    hack to make TestCase class being recognized as tests class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,14 @@ class TestCase extends PHPUnit_Framework_TestCase
     {
         self::$test_files = __DIR__ . '/../min_unit_tests/_test_files';
     }
+
+    /**
+     * Excluding from phpunit.xml does not work, even using dir,
+     * hence this dummy test.
+     *
+     * @link http://stackoverflow.com/q/2736343/2314626
+     */
+    public function test_does_nothing() {
+
+    }
 }
