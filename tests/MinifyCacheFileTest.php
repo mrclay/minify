f71dd4c79f06a09a79dd346019f7921a4d77d12b
Elan Ruusamäe <glen@delfi.ee>
2015-11-19 18:52:49 +0200

    move common cache test assertion to TestCase

--- tests/MinifyCacheFileTest.php
+++ tests/MinifyCacheFileTest.php
@@ -22,19 +22,4 @@ class MinifyCacheFileTest extends TestCase
 
         $this->assertTestCache($cache, $id, $data);
     }
-
-    private function assertTestCache(Minify_Cache_File $cache, $id, $data)
-    {
-        $this->assertTrue($cache->store($id, $data), "$id store");
-        $this->assertEquals($cache->getSize($id), $this->countBytes($data), "$id getSize");
-        $this->assertTrue($cache->isValid($id, $_SERVER['REQUEST_TIME'] - 10), "$id isValid");
-
-        ob_start();
-        $cache->display($id);
-        $displayed = ob_get_contents();
-        ob_end_clean();
-
-        $this->assertSame($data, $displayed, "$id display");
-        $this->assertEquals($data, $cache->fetch($id), "$id fetch");
-    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,4 +33,26 @@ class TestCase extends PHPUnit_Framework_TestCase
     {
 
     }
+
+    /**
+     * Common assertion for cache tests.
+     *
+     * @param Minify_CacheInterface $cache
+     * @param string $id
+     * @param string $data
+     */
+    protected function assertTestCache(Minify_CacheInterface $cache, $id, $data)
+    {
+        $this->assertTrue($cache->store($id, $data), "$id store");
+        $this->assertEquals($cache->getSize($id), $this->countBytes($data), "$id getSize");
+        $this->assertTrue($cache->isValid($id, $_SERVER['REQUEST_TIME'] - 10), "$id isValid");
+
+        ob_start();
+        $cache->display($id);
+        $displayed = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame($data, $displayed, "$id display");
+        $this->assertEquals($data, $cache->fetch($id), "$id fetch");
+    }
 }
