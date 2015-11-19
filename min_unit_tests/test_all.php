3a9687ad4e0f99c2047218dc6a5c83ca5b950ace
Elan Ruusamäe <glen@delfi.ee>
2015-11-19 19:32:38 +0200

    port MinifyCSSUriRewriterTest to phpunit

--- min_unit_tests/test_all.php
+++ min_unit_tests/test_all.php
@@ -2,7 +2,6 @@
 
 require 'test_Minify.php';
 require 'test_Minify_CSS.php';
-require 'test_Minify_CSS_UriRewriter.php';
 require 'test_Minify_Lines.php';
 require 'test_HTTP_ConditionalGet.php';
 require 'test_environment.php';
