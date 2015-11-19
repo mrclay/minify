913f6716670b50f223fa7daab6488eb8f28616dc
Elan Ruusamäe <glen@delfi.ee>
2015-11-19 19:21:32 +0200

    port MinifyHTMLTest to phpunit

--- min_unit_tests/test_all.php
+++ min_unit_tests/test_all.php
@@ -3,7 +3,6 @@
 require 'test_Minify.php';
 require 'test_Minify_CSS.php';
 require 'test_Minify_CSS_UriRewriter.php';
-require 'test_Minify_HTML.php';
 require 'test_Minify_Lines.php';
 require 'test_HTTP_ConditionalGet.php';
 require 'test_environment.php';
---
+++ tests/MinifyHTMLTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class MinifyHTMLTest extends TestCase
+{
+    public function test1()
+    {
+        $src = file_get_contents(self::$test_files . '/html/before.html');
+        $minExpected = file_get_contents(self::$test_files . '/html/before.min.html');
+
+        $minOutput = Minify_HTML::minify($src, array(
+            'cssMinifier' => array('Minify_CSSmin', 'minify'),
+            'jsMinifier' => array('JSMin\\JSMin', 'minify'),
+        ));
+
+        $this->assertEquals($minExpected, $minOutput);
+    }
+
+    public function test2()
+    {
+        $src = file_get_contents(self::$test_files . '/html/before2.html');
+        $minExpected = file_get_contents(self::$test_files . '/html/before2.min.html');
+
+        $minOutput = Minify_HTML::minify($src, array(
+            'cssMinifier' => array('Minify_CSSmin', 'minify'),
+            'jsMinifier' => array('JSMin\\JSMin', 'minify'),
+        ));
+
+        $this->assertEquals($minExpected, $minOutput);
+    }
+}
