2d2e06f4382512cd21590d17e4f98e9390473b22
Steve Clay <steve@mrclay.org>
2016-10-02 05:37:07 -0400

    Add temp location to server-info.php

--- min/server-info.php
+++ min/server-info.php
@@ -16,10 +16,13 @@ if (!$enabled) {
 header('Content-Type: text/plain');
 
 $file = __FILE__;
+$tmp = sys_get_temp_dir();
+
 echo <<<EOD
 __FILE__        : $file
 SCRIPT_FILENAME : {$_SERVER['SCRIPT_FILENAME']}
 DOCUMENT_ROOT   : {$_SERVER['DOCUMENT_ROOT']}
 SCRIPT_NAME     : {$_SERVER['SCRIPT_NAME']}
 REQUEST_URI     : {$_SERVER['REQUEST_URI']}
+Cache directory : $tmp
 EOD;
