cf9872a04472d56463d9d00b5a98c3584c6dd417
Evgeny Murashkin <rusjoan@users.noreply.github.com>
2017-01-19 12:42:35 +0300

    Update CSSmin.php to satisfy PHP 7.1 requirements
    For now PHP 7.1 gives "A non well formed numeric value encountered" if we're trying to multiply string with number.
    It's all about strict typecasting.

--- min/lib/CSSmin.php
+++ min/lib/CSSmin.php
@@ -765,7 +765,9 @@ class CSSmin
     private function normalize_int($size)
     {
         if (is_string($size)) {
-            switch (substr($size, -1)) {
+            $letter = substr($size, -1);
+            $size = intval($size);
+            switch ($letter) {
                 case 'M': case 'm': return $size * 1048576;
                 case 'K': case 'k': return $size * 1024;
                 case 'G': case 'g': return $size * 1073741824;
