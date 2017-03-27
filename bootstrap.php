6a2fed39126ed4cb1bd0a72d47539f2b56eee444
srggroup <adam.balint@srg.hu>
2017-03-27 09:28:00 +0200

    Composer compatibility - check if library is inside the vendor dir

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,12 @@
  */
 
 call_user_func(function () {
-    $file = __DIR__ . '/vendor/autoload.php';
+	$vendorDir = __DIR__ . '/vendor';
+	if(file_exists(__DIR__ . '/../../../vendor')){
+		//Used as a composer library
+		$vendorDir = __DIR__ . '/../../../vendor';
+	}
+	$file = $vendorDir . '/autoload.php';
     if (!is_file($file)) {
         echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
             'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
