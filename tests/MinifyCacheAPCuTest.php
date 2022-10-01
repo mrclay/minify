455084b2843ad1565401cb4b3bb0d89e2b055649
Christoph Rönsch <christoph.roensch@arcor.de>
2022-10-01 22:23:52 +0200

    Minify_Cache_APCu replaces APC

--- tests/MinifyCacheAPCuTest.php
+++ tests/MinifyCacheAPCuTest.php
@@ -2,15 +2,16 @@
 
 namespace Minify\Test;
 
-use Minify_Cache_APC;
+use Minify_Cache_APCu;
 
-class MinifyCacheAPCTest extends TestCase
+class MinifyCacheAPCuTest extends TestCase
 {
     public function setUp()
     {
-        if (!function_exists('apc_store')) {
-            $this->markTestSkipped("To test this component, install APC extension");
+        if (!function_exists('apcu_store')) {
+            $this->markTestSkipped("To test this component, install APCu extension");
         }
+        ini_set('apc.enable_cli', 1);
     }
 
     public function test1()
@@ -18,7 +19,7 @@ class MinifyCacheAPCTest extends TestCase
         $data = str_repeat(md5(time()) . 'í', 100); // 3400 bytes in UTF-8
         $id = 'Minify_test_cache';
 
-        $cache = new Minify_Cache_APC();
+        $cache = new Minify_Cache_APCu();
         $this->assertTestCache($cache, $id, $data);
     }
 }
